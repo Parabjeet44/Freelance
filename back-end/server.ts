98aace66f78fa515fe09b74f44a5b9cb1f08898e
Parabjeet Singh <gurdeepprajpat@gmail.com>
2025-05-26 00:07:08 +0530

    Changed the cors

--- back-end/server.ts
+++ back-end/server.ts
@@ -10,7 +10,7 @@ dotenv.config();
 const app=express();
 
 app.use(cors({
-    origin:`${process.env.CORS_URI}`,
+    origin:`https://freelance-czye.vercel.app/`,
     credentials: true,
 }));
 app.use(cookieParser());
